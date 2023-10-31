fda0e8de8084088d5866f02cf43866824c8ec884
ChaoII <849453582@qq.com>
2023-10-31 17:31:19 +0800

    fix some error

--- getBestCase.m
+++ getBestCase.m
@@ -1,4 +1,4 @@
-function [mix_case,mix_info,mix_price] = getBestCase(coalInfo, unitConstraint, containerConstraint, feederCapacity, mixRatio, mutexCoal, standCoalQty, maxMixCoal)   
+function [mix_case,mix_info,mix_price] = getBestCase(coalInfo, unitConstraint, containerConstraint, feederCapacity, mixRatio, mutexCoal, standCoalQty, maxMixCoal)
     %%
     % 功能：由给定备选上仓煤种，在满足机组约束、煤仓约束和比例约束条件下，生成若干最优配仓方案（原煤均价最低）;
     % 输入：以给定备选 n 种煤，配煤比例 k 种，机组共 m 个仓，生成top s 个最优解为例：
@@ -33,14 +33,14 @@ function [mix_case,mix_info,mix_price] = getBestCase(coalInfo, unitConstraint, c
     %       （4）在编译为dll时，请确保mcr版本、cplex版本、c++编译器版本及编译平台位数一致，否则无法被调用。
     %% ------------------------
     save("mock10.mat","coalInfo", "unitConstraint", "containerConstraint", "feederCapacity", "mixRatio", "mutexCoal", "standCoalQty", "maxMixCoal");
-    display(coalInfo);
-    display(unitConstraint);
-    display(containerConstraint);
-    display(feederCapacity);
-    display(mixRatio);
-    display(mutexCoal);
-    display(standCoalQty);
-    display(maxMixCoal);
+%     display(coalInfo);
+%     display(unitConstraint);
+%     display(containerConstraint);
+%     display(feederCapacity);
+%     display(mixRatio);
+%     display(mutexCoal);
+%     display(standCoalQty);
+%     display(maxMixCoal);
     mixRatio = mixRatio./(gcd(mixRatio(:,1),mixRatio(:,2))*ones(1,2));
     sum_mixRatio = sum(mixRatio, 2);
     slcm = 1;
@@ -49,69 +49,86 @@ function [mix_case,mix_info,mix_price] = getBestCase(coalInfo, unitConstraint, c
     end
     ele_m = mixRatio./repmat(sum_mixRatio, 1, 2)*slcm;
     ele = unique(ele_m);
-    
     % 煤仓数量
     m = size(containerConstraint,1);
+    % 仓库启用标志
+    container_flag = containerConstraint(:,1);
     % 煤种数量
     n = size(coalInfo, 1);
-    x = intvar(m, n);
-    % 总煤量 煤仓数 * 单仓量(slcm个单位)
-    total_quality = slcm* m;
-    % 约束0煤的数量不能为负
-    constrat0 = x >= zeros(m, n);
-    % 约束1煤仓给煤机出力约束 所有仓同一时间煤量相同，都是12个单位
-    constrat1 = sum(x, 2)==slcm.*ones(m, 1);
-    % 约束2最多允许两种煤上仓，也就是每一行中非0的数小于等于2
-    constrat2 = [];
+    x = intvar(m, n,'full');
+    % *************总煤量 启用的煤仓数 * 单仓量(slcm个单位)************
+    total_quality = slcm* nnz(container_flag);
+    % 约束0: 煤的数量不能为负
+    constraint0 = x >= zeros(m, n);
+    % 约束1: 煤仓给煤机出力约束 所有仓同一时间煤量相同，都是12个单位
+    constraint1 = sum(x, 2)==slcm.*ones(m, 1).*container_flag;
+    % 约束2: 最多允许两种煤上仓，也就是每一行中非0的数小于等于2
+    constraint2 = [];                                                
     for i=1:m
-        constrat2= [constrat2,nnz(x(i,:))<=2];
+        constraint2= [constraint2,nnz(x(i,:))<=2];
     end
-    % 约束3，必须是在允许的比例之间[1:1,1:2,1:3,0:1]
-    constrat3 = ismember(x, ele');
-    % 约束4煤量约束sum(x,1) 每一种煤的总量
-    constrat4 = sum(x,1)'/slcm <= coalInfo(:,2)/feederCapacity;
-    % 约束5锅炉煤质约束 sum(x,1)即为所有煤仓中煤种的量，均匀混合所有煤仓的煤后的媒质
+    % 约束3: 必须是在允许的比例之间[1:1,1:2,1:3,...]
+    constraint3 = ismember(x, ele');
+    % 约束4: 煤量约束sum(x,1)每一种煤的总量比例小于给煤出力占存煤量比例
+    constraint4 = sum(x,1)'/total_quality <= coalInfo(:,2)/feederCapacity;
+    % 约束5：锅炉煤质约束 sum(x,1)即为所有煤仓中煤种的量，均匀混合所有煤仓的煤后的煤质
     temp = (sum(x,1)*coalInfo(:,3:end-1)/total_quality)';
-    constrat5 = (unitConstraint(:,1)<=temp) & (temp<=unitConstraint(:,2));
-    % 约束6 煤仓煤质约束(单仓12个煤量单位)
+    constraint5 = (unitConstraint(:,1) <= temp) & (temp <= unitConstraint(:,2));
+    % 约束6：煤仓煤质约束(单仓12个煤量单位)
     lb_index = 7:2:17;
     ub_index = lb_index+1;
     temp = x*coalInfo(:,3:end-1)/slcm;
-    constrat6 = (containerConstraint(:,lb_index) <= temp) & (temp <= containerConstraint(:,ub_index));
-    % 约束7 煤仓固定挥发分约束
-    constrat7=[];
-    % 约束8 煤仓固定比例约束
+%     constraint6=[];
+    constraint6 = (containerConstraint(:,lb_index).*container_flag <= temp) & (temp <= containerConstraint(:,ub_index).*container_flag);
+    % 约束7：煤仓固定挥发分约束
+    constraint7=[];
+    % 约束8：煤仓启用约束（未启用的话，煤仓未存煤）
+    [r,~] = find(containerConstraint(:,1)==0);
+    constraint8 = sum(x(r,:),2)==0;
+%     constraint8 = [];
+    % 约束9：煤仓固定比例约束
+    constraint9 = [];
     container_coal = containerConstraint(:,3:6);
     [r,~] = find(sum(container_coal,2)~=0);
-    constrat8 = x(container_coal(r,[1,2]))==container_coal(r,[3,4])*slcm;
-    % 约束9 互斥性约束，在所有煤种中有a不能有b
-    constrat9=[];
+    r_len = length(r);
+    for i =1:r_len
+         ttt = slcm ./ sum(container_coal(r(i),[3,4]));
+         constraint9 = [constraint9, x(r(i),container_coal(r(i),[1,2])) == container_coal(r(i),[3,4])*ttt];
+    end
+%     constraint9 = x(container_coal(r,[1,2]))==container_coal(r,[3,4])*slcm;
+    % 约束10：互斥性约束，在所有煤种中有a不能有b
+    constraint10=[];
+%     for i=1:length(mutexCoal)
+%         size_len = length(mutexCoal{i});
+%         for j = 1:m
+%             constraint10 =[constraint10, sum(repmat(x(j,:),size_len,1)-repmat(mutexCoal{i}',1,n),'all')<=1];
+%         end
+%     end
     for i=1:length(mutexCoal)
-        size_len = length(mutexCoal{i});
         for j = 1:m
-            constrat9 =[constrat9, sum(repmat(x(j,:),size_len,1)-repmat(mutexCoal{i}',1,n),'all')<=1];
+            constraint10 =[constraint10, nnz(x(j,mutexCoal{i}))<=1];
         end
     end
 
-    % 约束10 热值守恒
-    constrat10=[];
-%     constrat10 = abs((sum(x,1)*coalInfo(:,3)/total_quality)'*feederCapacity-200000)<=standCoalQty*7000;
-%     constrat10 = standCoalQty*7000 == (sum(x,1)*coalInfo(:,3)/total_quality)'*feederCapacity;
-    % 约束11 最大煤种约束
-    constrat11=[];
-    constrat11 = nnz(sum(x,1))<= maxMixCoal; 
+    % 约束11：热值守恒
+    constraint11=[];
+%     constraint11 = abs((sum(x,1)*coalInfo(:,3)/total_quality)'*feederCapacity-200000)<=standCoalQty*7000;
+%     constraint11 = standCoalQty*7000 == (sum(x,1)*coalInfo(:,3)/total_quality)'*feederCapacity;
+    % 约束12：最大煤种约束
+    constraint12 = nnz(sum(x,1)) <= maxMixCoal; 
     %目标函数(1XN)x(1XN)
     obj = sum(x,1)*coalInfo(:,end);
-    Constraints = [constrat0, constrat1, constrat2, constrat3, constrat4, constrat5, constrat6, constrat7, constrat8, constrat9, constrat10, constrat11];
+    Constraints = [constraint0, constraint1, constraint2, constraint3, constraint4, constraint5, constraint6, constraint7, constraint8, constraint9, constraint10, constraint11,constraint12];
     % 创建优化模型
-    ops = sdpsettings('solver','cutsdp', 'verbose', 0,'savesolveroutput', 2, 'savesolverinput', 1,'allownonconvex', 1); % 选择整数规划求解器
-    ops.cutsdp.SolCount = 3 ;
+    ops = sdpsettings('solver','mosek', 'verbose', 1,'savesolveroutput', 2, 'savesolverinput', 1,'allownonconvex', 1); % 选择整数规划求解器
+    ops.cutsdp.SolCount = 3;
     res= optimize(Constraints, obj, ops);
     display(res);
     display(res.info);
     if(res.problem==0)
         % 获取解
         solution = uint8(value(x));
+        disp(solution);
         gcd_matrix = ones(1,m,'uint8');
         for i =1 :m
             tmp = solution(i,solution(i,:)~=0);
